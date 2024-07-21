d0b96f2d4160aee99c975574128b349c973855c5
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-21 03:18:05 -0500

    Create cuadrilatero.js

---
+++ plugins/cuadrilatero.js
@@ -0,0 +1,41 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.cuadrilatero);
+};
+handler.command = /^(cuadrilatero|cuadrilatero)$/i;
+export default handler;
+handler.admin = true
+handler.group = true
+handler.botAdmin = true
+
+global.cuadrilatero = ` 
+CUADRILATERO
+⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨⤨
+
+⏰┨𝐇𝐎𝐑𝐀: 
+👕┨𝐕𝐄𝐒𝐓𝐈𝐌𝐄𝐍𝐓𝐀: 
+🛟┨𝐂𝐀𝐒𝐈𝐋𝐋𝐀𝐒:
+
+Escuadra 1: 
+➢⚜️
+➢⚔️
+➢⚔️
+➢⚔️
+
+Escuadra 2: 
+➢⚜️ 
+➢⚔️
+➢⚔️
+➢⚔️
+
+Escuadra 3: 
+➢⚜️ 
+➢⚔️
+➢⚔️
+➢⚔️
+
+𝙎𝙐𝙋𝙇𝙀𝙉𝙏𝙀𝙎 Igual estar a la hora del vs en DC:
+
+➠
+➠
+➠❕
+`;
